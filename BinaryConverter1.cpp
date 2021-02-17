c63eeb1516c63be6a45eb28cac2d2b3725176a23
ngonhatthien120 <76780335+ngonhatthien120@users.noreply.github.com>
2021-02-17 08:13:01 +0700

    Chuyển chuỗi thành mã nhị phân
    https://codelearn.io/training/detail/3162602

--- BinaryConverter1.cpp
+++ BinaryConverter1.cpp
@@ -18,6 +18,23 @@ string textToBinary(string text)
     ans.pop_back();
     return ans;
 }
+//Dùng for each(vẫn bị quá thời gian)
+string textToBinary_(string text)
+{
+    string res;
+    string ans;
+    for (char i : text) {
+        for (int j = 0; j < 8; j++) {
+            if (i & 1) res.insert(0, "1");
+            else res.insert(0, "0");
+            i = i >> 1;
+        }
+            ans += res + ' ';
+            res = "";        
+    }
+    ans.pop_back();
+    return ans;
+}
 int main()
 {
     string s;
