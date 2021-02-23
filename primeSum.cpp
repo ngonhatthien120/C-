2c052fb40c1aa2944d1837c5e1fc427f4a76c059
ngonhatthien120 <76780335+ngonhatthien120@users.noreply.github.com>
2021-02-23 15:36:18 +0700

    Số học(sàng Eratosthenes - số nguyên tố)
    Bài dùng thuật toán hay
    https://codelearn.io/learning/thuat-toan-can-ban/1438

--- primeSum.cpp
+++ primeSum.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+bool a[10000001];
+void prime(int n)
+{
+    a[0] = a[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        a[i] = 1;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (a[i])
+            for (int j = 2 * i; j <= n; j += i) {
+                a[j] = 0;
+            }
+    }
+}
+
+int primeSum(int n)
+{
+    int mod = 22082018;
+    prime(n);
+    int d = 0;
+    for (int i = 2; i <= n; i++)
+        if (a[i]) d = (d + i) % mod;
+    return d;
+}
+int main()
+{
+    int n ;
+    cin >> n;
+    cout << primeSum(n);
+    return 0;
+}
 
